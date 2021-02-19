112ed763ce7d4bf88e8e23691745be5900d67625
Dewa Bisma <idewagedebisma@gmail.com>
2021-02-19 19:20:49 +0800

    adding details components, styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Details from './components/Details/Details';
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
 
-const app = () => {
-  return <div>Hello!</div>;
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={classes.grid}
+      container
+      spacing={0}
+      alignItems='center'
+      justify='center'
+      style={{ height: '100vh' }}
+    >
+      <Grid item xs={12} sm={4}>
+        <Details title='Income' />
+      </Grid>
+
+      <Grid item xs={12} sm={4}>
+        Main
+      </Grid>
+
+      <Grid item xs={12} sm={4}>
+        <Details title='Expense' />
+      </Grid>
+    </Grid>
+  );
 };
 
-export default app;
+export default App;
